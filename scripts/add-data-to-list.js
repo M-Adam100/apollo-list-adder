d4f175d37fd5fa5c92a6f83bb6337d3c804f6101
Muhammad Adam <adam184688@gmail.com>
2022-02-24 23:57:10 +0500

    List Adding Fix

--- scripts/add-data-to-list.js
+++ scripts/add-data-to-list.js
@@ -7,7 +7,7 @@
     return new Promise(resolve => setTimeout(resolve, ms));
   }
 
-  function randomIntFromInterval(min, max) { // min and max included 
+  function randomIntFromInterval(min, max) {  
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
@@ -20,32 +20,24 @@
     return new Promise(async (resolve, reject) => {
       const sideBar = document.querySelector('.finder-explorer-sidebar-shown');
       const mainDiv = sideBar.querySelector('div').nextElementSibling
-      mainDiv.querySelector('[data-input="checkbox"]').click();
+      mainDiv.querySelector('[data-input="checkbox"]')?.click();
       await sleep(1000);
-      document.querySelector('div.apolloio-css-vars-reset.zp-overlay').querySelector('a').click()
+      document.querySelector('div.apolloio-css-vars-reset.zp-overlay')?.querySelector('a')?.click()
       await sleep(1000);
-      document.querySelector('.apollo-icon-plus').nextSibling.click();
+      document.querySelector('.apollo-icon-plus').nextSibling?.click();
       await sleep(300);
       const interval = setInterval(async () => {
-        const dialog = document.querySelector('.zp-modal-content').querySelector('.apolloio-css-vars-reset');
-        const input = dialog.querySelector('.Select.Select--multi.is-searchable').querySelector('input.Select-input');
+        const dialog = document.querySelector('.zp-modal-content')?.querySelector('.apolloio-css-vars-reset');
+        const input = dialog.querySelector('.Select.Select--multi.is-searchable')?.querySelector('input.Select-input');
         if (input && input!= '') {
           clearInterval(interval);
           await setInput(input, listName);
-          dialog.querySelector('[class="Select-option is-focused"]').click();
+          dialog.querySelector('[class="Select-option is-focused"]')?.click();
           await sleep(1000);
-          dialog.querySelector('[data-cy="confirm"]').click();
-          const intervalRefresh = setInterval(() => {
-            const notificationDiv = document.querySelector('.rrt-middle-container');
-            if(notificationDiv) {
-              const divs = [...notificationDiv.querySelectorAll('div')].filter(item => item.innerText.includes("Refresh prospects"));
-              if (divs.length > 0) {
-                clearInterval(intervalRefresh);
-                divs.reverse()[0].click();
-                resolve(true);
-              }
-            }
-          },500)
+          dialog.querySelector('[data-cy="confirm"]')?.click();
+          console.log('Next Page');
+          document.querySelector('img[src="https://app.apollo.io/images/nav-arrow-next-white.svg"]')?.click();
+          resolve(true);
         }
       }, 1000)
 		});
@@ -70,10 +62,11 @@
   const listName = CS.listName;
     if (pages > 0 && CS.listName) {
       for (let i = 1; i <= pages; i++) {
+        const delay = randomIntFromInterval(7, 15);
         console.log("Page # ", i);
         await addPeople();
-        console.log("Waiting for 2 Seconds")
-        await sleep(randomIntFromInterval(7, 15)*1000);
+        console.log(`Waiting for ${delay} Seconds`)
+        await sleep(delay * 1000);
       }
       alert('Finished Adding!');
     } else alert("Invalid PageNumber/ListName Number");
